9a5cf41af39d03cc17c79b2b39b4abe0079fd8f6
Marciokass <64607956+Marciokass@users.noreply.github.com>
2020-04-30 22:30:23 +0300

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("Laiškas negali būti išsiųstas: {} :x: ".format(member))
 
 
-client.run("")             
+client.run("NzA1NDg0MDA0MzQxNTc5ODA2.XqsnKA.1WgbfYoH0savvbuSxst-bepuEjA")
